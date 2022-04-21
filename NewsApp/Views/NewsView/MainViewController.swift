d9ef5a4c6aafdd29388d27f77f649a3106e88909
Morteza <morteza.ghrdi@gmail.com>
2022-04-21 11:21:25 +0430

    add loading view

--- NewsApp/Views/NewsView/MainViewController.swift
+++ NewsApp/Views/NewsView/MainViewController.swift
@@ -10,21 +10,37 @@ import UIKit
 class MainViewController: UIViewController, Storyboarded {
     
     @IBOutlet weak var newsTableView: UITableView!
+    
     var viewModel: MainViewModel?
     var news: [Int] = [] {
         didSet {
             newsTableView.reloadData()
         }
     }
-    
     var coordinator: NewsCoordinator?
+    
+    var loadingView: UIActivityIndicatorView = {
+        let actIndicator = UIActivityIndicatorView()
+        actIndicator.translatesAutoresizingMaskIntoConstraints = false
+        actIndicator.style = .large
+        return actIndicator
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
+        setupView()
         viewModel = MainViewModel()
         viewModel?.delegate = self
         viewModel?.fetchData()
+    }
+    
+    func setupView() {
+        self.view.addSubview(loadingView)
+        let loadingViewAnchors = [loadingView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+                                  loadingView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)]
+        NSLayoutConstraint.activate(loadingViewAnchors)
+        loadingView.startAnimating()
     }
     
     private func setupTableView() {
@@ -57,10 +73,12 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
 
 extension MainViewController: MainViewModelDelegate {
     func didFetchNews(news: [Int]) {
+        self.loadingView.stopAnimating()
         self.news = news
     }
     
     func didGetError(error: Error) {
+        self.loadingView.stopAnimating()
         let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
         self.navigationController?.present(alert, animated: true, completion: nil)
